88366b9df6f78c441941a9f45f9d8130d5bd7603
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-07-09 22:33:12 -0500

    revamped home page

--- js/main.js
+++ js/main.js
@@ -1,48 +1,50 @@
 "use strict";
 renderHTML()
 
+
 function renderHTML() {
     let html = "";
-    html += `<nav class="navbar  sticky-top navbar-expand-lg navbar-dark">
+    html += `
+<nav class="navbar  sticky-top navbar-expand-lg navbar-dark" style="align-content: center">
   <div class="container-fluid">
-    <a class="navbar-brand" style="color: ghostwhite" href="#">
-      <img src="../assets/newheadshot.PNG" alt="" width="100" height="100" style="border-radius: 100px">
-      christopher eliason</a>
+<!--    <a class="navbar-brand" style="color: black" href="#">-->
+<!--      <img src="../assets/newheadshot.PNG" alt="photo of christopher eliason" style="border-radius: 100px; height: 100px; width: 100px">-->
+<!--      christopher eliason</a>-->
      
        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-      <li class="nav-item col-3" >
-          <a class="nav-link " href="#"></a>
-        </li>
-        <li class="nav-item col-3" >
-          <a class="nav-link " href="#"></a>
-        </li>
-        <li class="nav-item col-3" >
-          <a class="nav-link " href="#"></a>
+      <ul class="navbar-nav mx-auto me-auto mb-2 mb-lg-0 ">
+         <li class="nav-item">
+          <a class="nav-link active" style="color: black; margin-right: 50px" href="#">(home)</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" style="color: ghostwhite; margin-right: 50px" href="#">projects</a>
+          <a class="nav-link active" style="color: black; margin-right: 50px" href="#">(projects)</a>
         </li>
-        <li class="nav-item ">
-          <a class="nav-link active" style="color: ghostwhite; margin-right: 50px" href="#">about</a>
+        <li class="nav-item">
+          <a class="nav-link active" style="color: black; margin-right: 50px" href="#">(about)</a>
         </li>
-        <li class="nav-item ">
-          <a class="nav-link active" style="color: ghostwhite; margin-right: 50px" href="#">contact</a>
+        <li class="nav-item">
+          <a class="nav-link active" style="color: black; margin-right: 50px" href="#">(contact)</a>
         </li>
       </ul>
     </div>
     </div>
 </nav>
-<hr style="color: black; margin-left: 2em; margin-right: 2em">
-<div class="content">
-
+<div class="welcome">
+<div class="jumbotron jumbotron-fluid">
+  <div class="container row">
+    <h1 class="display-4 col">christopher eliason</h1>
+    
+  </div>
+</div>
 </div>
 
 `
     $('#body').html(html)
+
+
     //
     // $('.about').on('click', function (e) {
     //     e.preventDefault();
